f2ee633ee758403c7a595095aa0e8b99edf3ddf9
Gennady Kashin <gennady.kashin94@gmail.com>
2023-02-26 11:50:04 +0300

    feat: add user in admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'first_name',
+        'last_name',
+    )
+    search_fields = ('username', 'role',)
+    list_filter = ('username',)
+    empty_value_display = '-пусто-'
+
+@admin.register(User)
